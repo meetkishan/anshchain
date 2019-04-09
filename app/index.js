fbe11448465afec2cb6b1247bc971f3d19377e32
kishank <kishankachchhi@gmail.com>
2019-04-09 16:24:31 +0530

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
+
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
+
+app.use(bodyParser.json());
+
+app.get('/blocks', (req, res) => {
+    res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+    // console.log(`Receiving : ${req.body.data}`);
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
+    p2pServer.syncChains();
+    res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+    // console.log(`Receiving : ${req.body.data}`);
+    const {recipient, amount} = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, bc, tp);
+    p2pServer.broadcastTransaction(transaction);
+    res.redirect('/transactions');
+});
+
+app.get('/mine-transactions', (req, res) => {
+    const block = miner.mine();
+    console.log(`New block added: ${block.toString()}`);
+    res.redirect('/blocks');
+});
+
+app.get('/public-key', (req, res) => {
+    res.json({publicKey: wallet.publicKey});
+});
+
+app.listen(HTTP_PORT, () => console.log(`Listeing on port ${HTTP_PORT}`));
+p2pServer.listen();
+
+//RUN command for instances HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
